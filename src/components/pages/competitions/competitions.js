f6c87d0dd21c6224ddbf9c91bd393e9881308738
KarovInal <inal.karov8@gmail.com>
2018-07-30 00:15:03 +0300

    Количество сревнований.
    Добавлено:
    - отображение колличества соревнований.
    
    Co-Authored-By: KarovInal <inal.karov8@gmail.com>

--- src/components/pages/competitions/competitions.js
+++ src/components/pages/competitions/competitions.js
@@ -1,14 +1,22 @@
 import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import {createStructuredSelector} from 'reselect';
+import { competitionsCountSelector } from 'Selectors/competition/competitions';
 import MainTemplate from 'Templates/main-template';
 import TopContent from 'Organisms/top-content';
 import CompetitionsList from 'Organisms/competitions-list';
 import { RegularText } from 'Atoms/fonts';
 import ComingSoon from 'Molecules/coming-soon';
 
+const stateToProps = createStructuredSelector({
+  competitionsCount: competitionsCountSelector
+});
+
+@connect(stateToProps)
 class Competitions extends Component {
   renderOrderCompetitions = () => (
     <RegularText>
-      ВСЕГО СОРЕВНОВАНИЙ: 12
+      ВСЕГО СОРЕВНОВАНИЙ: { this.props.competitionsCount }
     </RegularText>
   );
 
--- src/selectors/competition/competitions.js
+++ src/selectors/competition/competitions.js
@@ -1,1 +1,9 @@
+import { createSelector } from 'reselect';
+import { get } from 'lodash';
+
 export const competitionsSelector = state => state.competitions;
+
+export const competitionsCountSelector = createSelector(
+  competitionsSelector,
+  competitions => get(competitions, 'length', '')
+);
